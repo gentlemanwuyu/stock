86e9e1976860ff66f2c5a056095d5c6ca8d91d20
wuyu <492444775@qq.com>
2020-06-21 15:43:38 +0800

    修改获取指数日数据脚本

--- apps/source/management/commands/GetIndexDailyData.py
+++ apps/source/management/commands/GetIndexDailyData.py
@@ -6,57 +6,70 @@ from datetime import datetime
 from dateutil.relativedelta import relativedelta
 from apps.source.models import Index
 from apps.source.models import IndexDailyData
-from queue import Queue
-from threading import Thread
+import threading
 import tushare as ts
 import math
+import time
 
 
 class Command(BaseCommand):
     help = '获取指数日数据'
+    thread_num = 10  # 线程数
+    all = False  # 是否获取全部数据
+    indexes = []  # 命令行传入的指数代码
+    index_objs = []  # 指数列表
+    start_at = None  # 命令行传入的起始日期
+    end_at = None  # 命令行传入的结束时间
+    api_times_pm = 100  # api每分钟请求限制
+    thread_api_freq = 0  # 每个线程请求api的频率
 
     def __init__(self):
-        self.all = False
-        self.indexes = []
-        self.start_at = None
-        self.end_at = None
-        self.queue = Queue()
-        self.token = settings.TUSHARE_API_TOKEN
-        ts.set_token(self.token)
+        ts.set_token(settings.TUSHARE_API_TOKEN)
 
     def add_arguments(self, parser):
         # Named (optional) arguments
-        parser.add_argument('--all', action='store_true', dest='all', help='是否拉取所有数据')
+        parser.add_argument('--all', action='store_true', dest='all', help='是否拉取全部数据')
         parser.add_argument('--indexes', dest='indexes', help='拉取某些指数的数据')
         parser.add_argument('--start', dest='start_at', help='起始日期')
         parser.add_argument('--end', dest='end_at', help='结束日期')
+        parser.add_argument('--tn', dest='thread_num', help='线程数', type=int)
 
     def handle(self, *args, **options):
-        print('[' + datetime.now().strftime('%Y-%m-%d %H:%M:%S') + ']开始获取指数日数据')
+        print('[' + datetime.now().strftime('%Y-%m-%d %H:%M:%S') + ']获取指数日数据脚本开始：')
         # 初始化脚本参数
         self.init_params(args=args, options=options)
         try:
-            ts.set_token(self.token)
             # 查询符合条件的指数
             query = Index.objects
             if self.indexes:
                 query = query.filter(ts_code__in=self.indexes)
             indexes = query.all()
-            # 将指数写入队列
-            for index in indexes:
-                self.queue.put(index)
-            for i in range(10):
-                thread = Thread(target=self.cycle_get_queue)
-                thread.start()
+            self.index_objs = list(indexes)
+            # 多线程拉取数据
+            thread_list = []
+            for i in range(self.thread_num):
+                t = threading.Thread(target=self.cycle_get_queue)
+                thread_list.append(t)
+            for t in thread_list:
+                t.setDaemon(True)
+                t.start()
+            for t in thread_list:
+                t.join()
         except Exception as e:
             print('[Exception]' + str(e))
-        print('[' + datetime.now().strftime('%Y-%m-%d %H:%M:%S') + ']获取指数日数据结束')
+        print('[' + datetime.now().strftime('%Y-%m-%d %H:%M:%S') + ']获取指数日数据脚本结束。')
 
     def cycle_get_queue(self):
-        while True:
-            index = self.queue.get()
-            if index:
-                self.handle_one_index(index)
+        while 0 < self.index_objs.__len__():
+            stock = self.index_objs.pop(0)
+            if stock:
+                while True:
+                    try:
+                        self.handle_one_index(stock)
+                        break
+                    except IOError:
+                        print("请求频繁，睡眠10秒后继续请求")
+                        time.sleep(10)
             else:
                 return
 
@@ -80,15 +93,18 @@ class Command(BaseCommand):
         end_date = datetime.strptime(end_at, '%Y%m%d')
 
         while start_date.__le__(end_date):
+            while_start_at = datetime.now()
             after_30_years = start_date + relativedelta(years=30)
             if after_30_years.__lt__(end_date):
-                print(index.ts_code + index.name + '[' + start_date.strftime('%Y%m%d') + '~' + after_30_years.strftime(
+                print('[' + index.ts_code + ']' + index.name + '[' + start_date.strftime(
+                    '%Y%m%d') + '~' + after_30_years.strftime(
                     '%Y%m%d') + ']')
                 data = ts.pro_bar(ts_code=index.ts_code, asset='I', start_date=start_date.strftime('%Y%m%d'),
                                   end_date=after_30_years.strftime('%Y%m%d'))
                 start_date = after_30_years + relativedelta(days=1)
             else:
-                print(index.ts_code + index.name + '[' + start_date.strftime('%Y%m%d') + '~' + end_date.strftime(
+                print('[' + index.ts_code + ']' + index.name + '[' + start_date.strftime(
+                    '%Y%m%d') + '~' + end_date.strftime(
                     '%Y%m%d') + ']')
                 data = ts.pro_bar(ts_code=index.ts_code, asset='I', start_date=start_date.strftime('%Y%m%d'),
                                   end_date=end_date.strftime('%Y%m%d'))
@@ -114,12 +130,22 @@ class Command(BaseCommand):
                     item['amount'] = 0.00
                 IndexDailyData.objects.update_or_create(defaults=dict(item), ts_code=item['ts_code'],
                                                         trade_date=item['trade_date'])
+            time_interval = (datetime.now() - while_start_at).seconds + 1
+            if time_interval < self.thread_api_freq:
+                diff = self.thread_api_freq - time_interval
+                print('循环花了', time_interval, '秒，没有超过频率限制', self.thread_api_freq, '秒，睡眠', diff, '秒')
+                time.sleep(diff)
 
     def init_params(self, args, options):
         self.all = options['all']
+        if options['thread_num']:
+            self.thread_num = options['thread_num']
         if options['indexes']:
             self.indexes = options['indexes'].split(',')
         if options['start_at']:
             self.start_at = options['start_at']
         if options['end_at']:
             self.end_at = options['end_at']
+        # 计算每个线程请求api的频率
+        thread_times_pm = math.floor(self.api_times_pm / self.thread_num)
+        self.thread_api_freq = math.ceil(60 / thread_times_pm)
