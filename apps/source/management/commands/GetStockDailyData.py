346a0cfb701ea0a9964a43eaa05cd0c5b841524a
wuyu <492444775@qq.com>
2020-06-21 14:17:29 +0800

    修改获取股票日数据脚本

--- apps/source/management/commands/GetStockDailyData.py
+++ apps/source/management/commands/GetStockDailyData.py
@@ -3,8 +3,10 @@
 from django.core.management.base import BaseCommand
 from django.conf import settings
 from datetime import datetime
+from dateutil.relativedelta import relativedelta
 from apps.source.models import Stock
 from apps.source.models import StockDailyData
+from apps.source.models import TradeCalendar
 import tushare as ts
 import threading
 import math
@@ -12,15 +14,17 @@ import math
 
 class Command(BaseCommand):
     help = '获取股票日数据'
+    thread_num = 10  # 线程数
+    all = False  # 是否获取全部数据
+    start_at = None  # 命令行传入的开始日期
+    end_at = None  # 命令行传入的结束日期
+    stocks = []  # 命令行传入的股票代码
+    stock_objs = []  # 股票集合
+    last_date = ''  # 从api获取数据时默认的截止日期
+    last_trade_date = ''  # 上一个交易日
 
     def __init__(self):
-        self.all = False
-        self.stocks = []
-        self.stock_objs = []
-        self.start_at = None
-        self.end_at = None
-        self.token = settings.TUSHARE_API_TOKEN
-        ts.set_token(self.token)
+        ts.set_token(settings.TUSHARE_API_TOKEN)
 
     def add_arguments(self, parser):
         # Named (optional) arguments
@@ -28,21 +32,22 @@ class Command(BaseCommand):
         parser.add_argument('--stocks', dest='stocks', help='拉取某些股票的数据')
         parser.add_argument('--start', dest='start_at', help='起始日期')
         parser.add_argument('--end', dest='end_at', help='结束日期')
+        parser.add_argument('--tn', dest='thread_num', help='线程数', type=int)
 
     def handle(self, *args, **options):
-        print('[' + datetime.now().strftime('%Y-%m-%d %H:%M:%S') + ']开始获取股票日数据')
+        print('[' + datetime.now().strftime('%Y-%m-%d %H:%M:%S') + ']获取股票日数据脚本开始：')
         # 初始化脚本参数
         self.init_params(args=args, options=options)
         try:
-            ts.set_token(self.token)
             # 查询符合条件的股票
             query = Stock.objects
             if self.stocks:
                 query = query.filter(ts_code__in=self.stocks)
             stocks = query.all()
             self.stock_objs = list(stocks)
+            # 多线程拉取数据
             thread_list = []
-            for i in range(10):
+            for i in range(self.thread_num):
                 t = threading.Thread(target=self.cycle_get_queue)
                 thread_list.append(t)
             for t in thread_list:
@@ -52,7 +57,7 @@ class Command(BaseCommand):
                 t.join()
         except Exception as e:
             print('[Exception]' + str(e))
-        print('[' + datetime.now().strftime('%Y-%m-%d %H:%M:%S') + ']获取股票日数据结束')
+        print('[' + datetime.now().strftime('%Y-%m-%d %H:%M:%S') + ']获取股票日数据脚本结束。')
 
     def cycle_get_queue(self):
         while 0 < self.stock_objs.__len__():
@@ -68,16 +73,43 @@ class Command(BaseCommand):
         :param stock:
         :return:
         """
-        start_at = datetime.now().strftime('%Y%m%d')
+        # 获取全部数据
         if self.all:
-            start_at = stock.list_date
-        elif self.start_at:
+            return self.get_stock_all_data(stock)
+        # 获取上一个交易日的数据，判断是否要拉取全部数据
+        try:
+            stock_last_trade = StockDailyData.objects.filter(ts_code=stock.ts_code,
+                                                             trade_date=self.last_trade_date).get()
+        except StockDailyData.DoesNotExist:
+            return self.get_stock_all_data(stock)
+
+        data = ts.pro_bar(ts_code=stock.ts_code, adj='qfq', start_date=self.last_trade_date,
+                          end_date=self.last_trade_date)
+        if float(stock_last_trade.close) != data.loc[0]['close']:
+            print('数据库中上个交易日的数据与api获取的不一致')
+            return self.get_stock_all_data(stock)
+        # 如果传了start和end参数，则获取这个区间的数据，否则是获取上个交易日到截止日期的数据
+        start_at = self.last_trade_date
+        if self.start_at:
             start_at = self.start_at
-        end_at = datetime.now().strftime('%Y%m%d')
+        end_at = self.last_date
         if self.end_at:
             end_at = self.end_at
-        print(stock.ts_code + stock.name + '[' + start_at + '~' + end_at + ']')
-        data = ts.pro_bar(ts_code=stock.ts_code, adj='qfq', start_date=start_at, end_date=end_at)
+        print('[' + stock.ts_code + ']' + stock.name + '[' + start_at + '~' + end_at + ']')
+        self.get_stock_data(stock=stock, start_date=start_at, end_date=end_at)
+
+    def get_stock_all_data(self, stock):
+        start_at = stock.list_date
+        end_at = self.last_date
+        print('[' + stock.ts_code + ']' + stock.name + '，拉取股票全部数据[' + start_at + '~' + end_at + ']')
+        self.get_stock_data(stock=stock, start_date=start_at, end_date=end_at)
+
+    @staticmethod
+    def get_stock_data(stock, start_date, end_date):
+        data = ts.pro_bar(ts_code=stock.ts_code, adj='qfq', start_date=start_date, end_date=end_date)
+        if data is None:
+            print("数据为None")
+            return False
         for index, item in data.iterrows():
             if math.isnan(item['open']):
                 item['open'] = 0.00
@@ -101,6 +133,8 @@ class Command(BaseCommand):
                                                     trade_date=item['trade_date'])
 
     def init_params(self, args, options):
+        if options['thread_num']:
+            self.thread_num = options['thread_num']
         self.all = options['all']
         if options['stocks']:
             self.stocks = options['stocks'].split(',')
@@ -108,3 +142,15 @@ class Command(BaseCommand):
             self.start_at = options['start_at']
         if options['end_at']:
             self.end_at = options['end_at']
+        # 计算截止日期
+        today = datetime.now().strftime('%Y%m%d')
+        if datetime.now().__ge__(datetime.strptime(today + ' 17:00:00', '%Y%m%d %H:%M:%S')):
+            self.last_date = datetime.now().strftime('%Y%m%d')
+        else:
+            yesterday = datetime.now() - relativedelta(days=1)
+            self.last_date = yesterday.strftime('%Y%m%d')
+
+        trade_calendar = TradeCalendar.objects.filter(exchange='SSE', cal_date=today).get()
+        if not trade_calendar:
+            raise Exception("获取今天日历失败")
+        self.last_trade_date = trade_calendar.pretrade_date
