64add67583881897887a7414e18d64b2b5887acb
WuYu <492444775@qq.com>
2020-07-08 22:23:23 +0800

    获取指数列表脚本和获取股票列表脚本添加日志

--- apps/source/management/commands/GetStockList.py
+++ apps/source/management/commands/GetStockList.py
@@ -2,8 +2,10 @@
 
 from django.core.management.base import BaseCommand
 from django.conf import settings
+from datetime import datetime
 from apps.source.models import Stock
 import tushare as ts
+import logging
 
 
 class Command(BaseCommand):
@@ -26,15 +28,22 @@ class Command(BaseCommand):
     ]
 
     def __init__(self):
+        self.logger = logging.getLogger('log')
         self.token = settings.TUSHARE_API_TOKEN
 
     def handle(self, *args, **options):
         ts.set_token(self.token)
         pro = ts.pro_api()
 
+        self.log('[' + datetime.now().strftime('%Y-%m-%d %H:%M:%S') + '][GetStockList]脚本开始：')
         try:
             data = pro.stock_basic(list_status='L', fields=','.join(self.fields))
             for index, item in data.iterrows():
                 Stock.objects.update_or_create(defaults=dict(item), ts_code=item['ts_code'])
         except Exception as e:
-            print('程序出现异常:' + str(e))
+            self.log('程序出现异常:' + str(e))
+        self.log('[' + datetime.now().strftime('%Y-%m-%d %H:%M:%S') + '][GetStockList]脚本结束。')
+
+    def log(self, msg):
+        print(msg)
+        self.logger.info(msg)
