74e21f05e6aca340b322d1b469c1b5034f9385f9
WuYu <492444775@qq.com>
2020-07-08 23:15:25 +0800

    获取日历脚本添加日志

--- apps/source/management/commands/GetTradeCalendar.py
+++ apps/source/management/commands/GetTradeCalendar.py
@@ -2,9 +2,10 @@
 
 from django.core.management.base import BaseCommand
 from django.conf import settings
+from datetime import datetime
 from apps.source.models import TradeCalendar
 import tushare as ts
-import math
+import logging
 
 
 class Command(BaseCommand):
@@ -17,14 +18,22 @@ class Command(BaseCommand):
     ]
 
     def __init__(self):
+        self.logger = logging.getLogger('log')
         self.token = settings.TUSHARE_API_TOKEN
         ts.set_token(self.token)
 
     def handle(self, *args, **options):
         pro = ts.pro_api()
+
+        self.log('[' + datetime.now().strftime('%Y-%m-%d %H:%M:%S') + '][GetTradeCalendar]脚本开始：')
         try:
             data = pro.trade_cal(fields=','.join(self.fields))
             for index, item in data.iterrows():
                 TradeCalendar.objects.update_or_create(defaults=dict(item), exchange=item['exchange'], cal_date=item['cal_date'])
         except Exception as e:
-            print('程序出现异常:' + str(e))
+            self.log('程序出现异常:' + str(e))
+        self.log('[' + datetime.now().strftime('%Y-%m-%d %H:%M:%S') + '][GetTradeCalendar]脚本结束。')
+
+    def log(self, msg):
+        print(msg)
+        self.logger.info(msg)
