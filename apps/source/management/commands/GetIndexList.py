e724a914dbe0156452e162d5adad20574f4703ab
WuYu <492444775@qq.com>
2020-07-08 22:23:15 +0800

    获取指数列表脚本和获取股票列表脚本添加日志

--- apps/source/management/commands/GetIndexList.py
+++ apps/source/management/commands/GetIndexList.py
@@ -2,9 +2,11 @@
 
 from django.core.management.base import BaseCommand
 from django.conf import settings
+from datetime import datetime
 from apps.source.models import Index
 import tushare as ts
 import math
+import logging
 
 
 class Command(BaseCommand):
@@ -26,12 +28,14 @@ class Command(BaseCommand):
     ]
 
     def __init__(self):
+        self.logger = logging.getLogger('log')
         self.token = settings.TUSHARE_API_TOKEN
         ts.set_token(self.token)
 
     def handle(self, *args, **options):
         pro = ts.pro_api()
 
+        self.log('[' + datetime.now().strftime('%Y-%m-%d %H:%M:%S') + '][GetIndexList]脚本开始：')
         try:
             data = pro.index_basic(fields=','.join(self.fields))
             for index, item in data.iterrows():
@@ -39,4 +43,9 @@ class Command(BaseCommand):
                     item['base_point'] = 0.00
                 Index.objects.update_or_create(defaults=dict(item), ts_code=item['ts_code'])
         except Exception as e:
-            print('程序出现异常:' + str(e))
+            self.log('程序出现异常:' + str(e))
+        self.log('[' + datetime.now().strftime('%Y-%m-%d %H:%M:%S') + '][GetIndexList]脚本结束。')
+
+    def log(self, msg):
+        print(msg)
+        self.logger.info(msg)
